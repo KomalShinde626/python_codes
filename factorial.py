1485d2f16bb40d3d26932187326b3ead87af81c4
KomalShinde626 <115860674+KomalShinde626@users.noreply.github.com>
2022-10-15 13:14:10 +0530

    factorial

--- factorial.py
+++ factorial.py
@@ -6,7 +6,8 @@ factorial = 0
 # check if the number is negative, positive or zero
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
-#issue - if the num = 0 then its factorial is 1.
+elif the num == 0:
+   print("then its factorial is 1.")
 #add a condition for that.
 else:
    for i in range(1,num + 1):
